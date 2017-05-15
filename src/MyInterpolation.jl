9c6f33d35f04495fe72af3cc163bc7589e2dd7ad
HOME\一織 <ks.tat-chi_0109@docomo.ne.jp>
2017-05-15 13:37:35 +0900

    修正

--- src/MyInterpolation.jl
+++ src/MyInterpolation.jl
@@ -7,18 +7,18 @@ immutable MyLinInterp
     vals::Array
 end
 
-function (f::MyLinInterp)(points::MyLinInterp,x)
-    i = searchsortedlast(points.grid,x)
-    k = searchsortedfirst(points.grid,x)
-    if i == 0 || (i == length(points.grid) && k == length(points.grid) + 1)
+function (f::MyLinInterp)(x)
+    i = searchsortedlast(grid,x)
+    k = searchsortedfirst(grid,x)
+    if i == 0 || (i == length(grid) && k == length(grid) + 1)
         return 0
     end
 
-    if i == length(points.grid) && k != length(points.grid) + 1
-        return (points.vals[i])
+    if i == length(grid) && k != length(grid) + 1
+        return (vals[i])
     end
     
-    interpolated_value = (x - points.grid[i])*(points.vals[i+1]-points.vals[i])/(points.grid[i+1]-points.grid[i])+points.vals[i]
+    interpolated_value = (x - grid[i])*(vals[i+1]-vals[i])/(grid[i+1]-grid[i])+vals[i]
     return interpolated_value 
 end
 
